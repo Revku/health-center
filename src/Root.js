fb8b5ccaa938225de4cdd4c884e2d6934af38278
Revku <revkuu@gmail.com>
2022-04-10 13:48:05 +0200

    Add react router

---
+++ src/Root.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router'
+import Home from './views/Home'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Root
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Root from './Root';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
